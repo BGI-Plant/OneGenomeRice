4aca1f7c24302b4b17de85d8357d7e5b84289908
Yu XU <sein.tao@gmail.com>
2026-04-21 14:49:46 +0800

    add applications files

---
+++ applications/1.indica-japonica_introgression_analysis/run_train_rf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Runs Mixtral 1.2B model
+
+python3 scripts/train_rf.py --config config/train_rf_config.yaml
+
---
+++ applications/1.indica-japonica_introgression_analysis/run_variety_inference.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Runs Mixtral 1.2B model
+
+python3 scripts/variety_inference.py --config config/variety_inference_config.yaml
+
